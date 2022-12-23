67c053fc54e38d54b38f6f04cee1bebf24f529c2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-12-22 20:28:56 -0800

    Add changes to materials function

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,19 @@
 class Craft {
+    constructor(craftInfo){
+        this.name = craftInfo.type; 
+        this.materials = craftInfo.materials
+        this.completed = false
+    }
 
+    completeCraft() {
+        if (this.completed = true) {
+            return "All done! It looks great!";
+        }
+    }
+
+    calculateProjectTotal() {
+        
+    }
 }
 
 module.exports = Craft;
