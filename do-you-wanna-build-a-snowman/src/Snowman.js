ab5e53303fc04bdf7a949af9b1c32460f418ad67
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-12-31 17:29:45 -0800

    Add final functions to snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,7 +1,23 @@
 class Snowman {
-    constructor(carrots){
-        this.snowmanDetails = {carrots:carrots}
+  constructor(obj) {
+    this.carrots = obj.carrots;
+    this.coal = obj.coal;
+    this.buttons = obj.buttons;
+    this.snowballs = obj.snowballs;
+    this.magicHat = false;
+  }
+
+  canWearMagicHat(){
+      this.magicHat = true;
+      var isWearingHat = true;
+  if (this.coal < 2  || this.buttons < 5 || this.carrots < 1 || this.snowballs <2) {
+        this.magicHat = false
+        isWearingHat = false
+        return isWearingHat = false
+    } else {
+        return isWearingHat = true
     }
+  }  
 }
 
 module.exports = Snowman;
