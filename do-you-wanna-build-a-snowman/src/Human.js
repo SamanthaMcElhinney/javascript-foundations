c908154fab5cabc270817ca690fe64541fe576cd
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-01-01 17:57:10 -0800

    Add changes to do you want to build a snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,6 +1,11 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor(name, object){
+        this.name = name
+        this.wantsToBuildASnowman = true
+        this.materials = object.snowballs, object.coal, object.buttons, object.carrots
+    }
 
 }
 
